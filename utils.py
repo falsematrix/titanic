c5ab5925fa17948bcce6f83574c96c6d12c8070a
ArbaazBeg15 <31arbazk31@gmail.com>
2026-02-20 20:08:59 +0530

    .

--- utils.py
+++ utils.py
@@ -1,3 +1,4 @@
 def fillna_with_mean(df, cols):
-    df[cols] = df[cols].fillna(value=df[cols].mean())
-    return df
+    #df[cols] = df[cols].fillna(value=df[cols].mean())
+    #return df
+    print("hihihi")
